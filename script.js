d4eef090312b216263702f38dde74fefe36f8e80
thegirlcoderr <lynndy2015@gmail.com>
2023-07-20 10:15:40 +0100

    added error handling to fetch request

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ function generateUser() {
    showSpinner()
 
      fetch('https://randomuser.me/api')
-        .then((res) => res.json())
+       .then((res) => {
+
+         if (!res.ok) {
+           throw new Error ('Request Failed')
+         }
+
+         
+         return res.json()
+       })
          .then((data) => {
               hideSpinner()
              displayUser(data.results[0]);
             
-         });
+         }).catch((error) => {
+           hideSpinner()
+           document.querySelector('#user').innerHTML = `
+           <p class ="text-xl text-center text-red-500 mb-5"> ${error} </p>`;
+         })
     
 }
 
